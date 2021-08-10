775384a9024e6f175919d15c1f0fd29fbfcbe850
Haohao-555 <3283627490@qq.com>
2021-08-10 19:56:45 +0800

    Site updated: 2021-08-10 19:56:36

--- js/title.js
+++ js/title.js
@@ -1,1 +1,15 @@
-"use strict";var titleTime,OriginTitle=document.title;document.addEventListener("visibilitychange",function(){document.hidden?($('[rel="icon"]').attr("href","/img/favicon.ico"),document.title="ヽ(●-`Д´-)ノ你丑你就走！",clearTimeout(titleTime)):($('[rel="icon"]').attr("href","/img/favicon.ico"),document.title="ヾ(Ő∀Ő3)ノ你帅就回来！"+OriginTitle,titleTime=setTimeout(function(){document.title=OriginTitle},2e3))});
+ var OriginTitle = document.title;
+ var titleTime;
+ document.addEventListener('visibilitychange', function() {
+     if (document.hidden) {
+         $('[rel="icon"]').attr('href', "/img/favicon.ico");
+         document.title = 'ヽ(●-`Д´-)ノ你丑你就走！';
+         clearTimeout(titleTime);
+     } else {
+         $('[rel="icon"]').attr('href', "/img/favicon.ico");
+         document.title = 'ヾ(Ő∀Ő3)ノ你帅就回来！' + OriginTitle;
+         titleTime = setTimeout(function() {
+             document.title = OriginTitle;
+         }, 2000);
+     }
+ });
